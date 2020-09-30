361010993d690c4774f79cd6606ee2cdb1cb6d56
hopegipson <gipson.hope@gmail.com>
2020-09-29 22:39:53 -0600

    updated config.ru file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,44 @@ class UsersController < ApplicationController
 
     get '/login' do
         if logged_in?
+            redirect "/users/#{current_user.id}"
             #redirect to the current user page
         else
-            #send to login view
+            #redirect to your login view form
+            erb:'/users/login.html'
         end
     end
+
+    post '/login' do
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session [:user_id] = user.id
+            #going to need to make a users show page for each id call
+            redirect "/users/#{user.id}"
+        else
+            redirect "/login"
+        end
+    end
+
+    get '/users/:id' do
+        @user = User.find_by(id: params[:id])
+        redirect back unless @user
+        @ghosts = @user.ghosts
+        erb :'users/show.html'
+    end
+
+    get '/signup' do
+        if logged_in?
+            redirect "/users/#{current_user.id}"
+        else
+            erb: '/users/signup.html'
+        end
+    end
+
+    get '/users' do
+        erb :'/users/all.html'
+    end
+
+    post '/signup' do
+        identical = !!User.all.detect {|user| user.username == params[:username]}
 end
--- config.ru
+++ config.ru
@@ -1,9 +1,8 @@
 require './config/environment'
 
-if ActiveRecord::Migrator.needs_migration?
-  raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
-end
+raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.' if ActiveRecord::Migrator.needs_migration?
 
+# for submitting forms w/ patch, delete etc.
 use Rack::MethodOverride
 use CitiesController
 use GhostsController
