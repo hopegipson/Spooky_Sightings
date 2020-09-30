c829a45913eed4822a7e4ae50e32c2ba9e38a671
hopegipson <gipson.hope@gmail.com>
2020-09-29 22:56:11 -0600

    added user model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
-end
+    has_many :user_ghosts
+    has_many :ghosts, through: :user_ghosts
+    has_many :cities, through: :ghosts
+  end
+  
