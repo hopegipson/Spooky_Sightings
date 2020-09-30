f0e56b82f38051a00d814e59ff7231f0a9f29c23
hopegipson <gipson.hope@gmail.com>
2020-09-30 01:16:23 -0600

    updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,15 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
   configure do
     enable :sessions
     set :public_folder, 'public'
     set :views, 'app/views'
-    set :session_secret,
-    ENV['SESSION_SECRET']
+    set :session_secret, ENV['SESSION_SECRET']
   end
 
-  get "/" do
-    erb:'/cities/index.html'
+  get '/' do
+    redirect :cities
   end
 
   helpers do
@@ -20,12 +18,11 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      User.find_by(id:session[:user_id])
+      User.find_by(id: session[:user_id])
     end
 
     def session_info
       session
     end
   end
-
 end
