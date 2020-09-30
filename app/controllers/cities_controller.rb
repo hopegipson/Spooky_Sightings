09775d65d6a4c6f38c60b37a5e7a9094c08722b4
hopegipson <gipson.hope@gmail.com>
2020-09-30 01:15:20 -0600

    created cities controller

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,40 @@
-class CitiesController < Sinatra::Base
-end
+class CitiesController < ApplicationController
+    get '/cities/new' do
+      erb :'/cities/new.html'
+    end
+  
+    post '/cities' do
+        city = City.new(name: params[:name])
+  
+        if params.values.any?(&:empty?)
+         redirect '/cities/new'
+        elsif City.find_by(name: params[:name])
+            redirect "/cities/#{city.id}"
+        else
+            city.save
+            redirect "/cities/#{city.id}"
+        end
+    end
+  
+    get '/cities/:id' do
+      @city = City.find_by(id: params[:id])
+      @ghosts = @city.ghosts
+  
+      if @city
+        if @ghosts != nil
+          @creators= @ghosts.map { |ghost| user_id = ghost[:creator_id]
+          User.all.find_by(user_id)}    
+        end
+        erb :'/cities/show.html'
+      else
+       redirect :'cities/new' 
+      end
+    end
+  
+  
+    get '/cities' do
+      @cities = City.all
+      erb :'/cities/index.html'
+    end
+  end
+  
